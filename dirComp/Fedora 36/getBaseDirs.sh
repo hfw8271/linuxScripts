2b980941271b330f8a7da231c884cdbe8d8ac2b1
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2023-10-13 15:46:08 -0400

    Modifications Made

---
+++ dirComp/Fedora 36/getBaseDirs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dirs=("/bin" "/etc" "/opt" "/sbin" "/usr" "/var")
+
+getDirs() {
+    local dir="$1"
+    local out="$2"
+
+    for item in "$dir"/*; do
+        if [ -d "$item" ]; then
+            echo "$item" >> "$out"
+            getDirs "$item" "$out"
+        elif [ -f "$item" ]; then
+            echo "$item" >> "$out"
+        fi
+    done
+}
+
+for dir in "${dirs[@]}"; do
+    if [ -d "$dir" ]; then
+        out="${dir/\//}_base.txt"
+        echo "Processing Dir: $dir"
+        getDirs "$dir" "$out"
+        echo "Output saved to: $out"
+    else
+        echo "Directory not found: $dir"
+    fi
+done
